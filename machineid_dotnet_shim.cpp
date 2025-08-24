a7b72e1dcf3ee824eeb86f99df9479e22c89aab2
Hieu Dam <dhhieu113@gmail.com>
2025-08-24 11:37:40 +0700

    fix build

--- machineid_dotnet_shim.cpp
+++ machineid_dotnet_shim.cpp
@@ -1,4 +1,5 @@
 #include "machineid/machineid.h"
+#include <cstring>
 
 #ifdef _WIN32
 #define EXPORT __declspec(dllexport)
@@ -10,10 +11,17 @@ extern "C" {
     // This function returns a C-compatible string pointer that can be marshaled to .NET
     EXPORT const char* MachineHash() {
         std::string hash = machineid::machineHash();
+#ifdef _WIN32
+        // Allocate memory for the string that will be returned to .NET
+        char* result = new char[hash.length() + 1];
+        strcpy_s(result, hash.length() + 1, hash.c_str());
+        return result;
+#else
         // Allocate memory for the string that will be returned to .NET
         char* result = new char[hash.length() + 1];
         strcpy(result, hash.c_str());
         return result;
+#endif
     }
 
     // This function frees the memory allocated by MachineHash
